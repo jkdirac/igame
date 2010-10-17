fe745f8ebf3ee323b2e1bb14c8695170529c3433
jiangkun <jkdirac@email.ustc.edu.cn>
2010-10-17 14:45:26 +0800

    modify raptor lib

--- src/ExternalCode/raptor-1.4.19/src/raptor_config.h
+++ src/ExternalCode/raptor-1.4.19/src/raptor_config.h
@@ -310,7 +310,7 @@
 
 /* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
    `char[]'. */
-/* #undef YYTEXT_POINTER */
+#define YYTEXT_POINTER 1
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
