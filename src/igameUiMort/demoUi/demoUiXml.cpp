45f5f1e5a044f31abc2298168dde6fa0e7226486
jiangkun <jkdirac@email.ustc.edu.cn>
2010-10-24 01:00:41 +0800

    modify cmakelists.txt

--- src/igameUiMort/demoUi/demoUiXml.cpp
+++ src/igameUiMort/demoUi/demoUiXml.cpp
@@ -21,12 +21,14 @@
  */
  
 #include <MosQt>
+#include "IdSelWidget.h"
+#include "SceneViewWidget.h"
 
 namespace uiObjects {
 
 MView * mainView;
 MScene * mainScene;
-
+MWidget * w;
 };
 
 using namespace uiObjects;
@@ -37,20 +39,36 @@ BEGIN(demoUiXml)
 
     mainView->setWindowTitle("demoUiXml");
     mainView->showMaximized();
-    mainView->setSceneRect(0, 0, QApplication::desktop()->width(), QApplication::desktop()->height());
+	mainView->setSceneRect(0, 0, QApplication::desktop()->width(), QApplication::desktop()->height());
     mainView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     mainView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     mainScene = new MScene();
-    mainScene->loadXml("./demoUiXml.ui.xml");
+    mainScene->loadXml(":demoUiXml.ui.xml");
+	mainScene->setBackgroundBrush(QPixmap(":/images/demobg.png"));
 
-    QTime time;
-    time.restart();
-    while (time.elapsed() < 5000) {
-	}
+//    QTime time;
+//    time.restart();
+//    while (time.elapsed() < 5000) {
+//    }
+
+	MWidget* widget[100];
+	// 创建MWidget界面对象
+	// MWidget界面对象是用于Qt原生widget的容器
+	// 可以通过MWidget向场景中添加任何Qt原生widget
+	//
+	
+	IdSelWidget* IdSel = new IdSelWidget(NULL);
+	widget[1] = (MWidget*)mainScene->addWidget(IdSel);
+	widget[1]->setX(0);
+	widget[1]->setY(0);
+
+	SceneViewWidget* sceneView = new SceneViewWidget(NULL);
+	widget[2] = (MWidget*)mainScene->addWidget(sceneView);
+	widget[2]->setX(0);
+	widget[2]->setY(300);
 
     mainView->setScene(mainScene);
-
 }
 END(demoUiXml)
 
